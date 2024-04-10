3894ba3afad65007708899fa807a0b624b9f1d21
Innocent Ngene <ngeneinnocent@gmail.com>
2024-04-10 16:40:02 +0100

    landing page improvements and unsubscribe page

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const forms = document.querySelectorAll('.get-notified');
+
+  forms.forEach(form => {
+    const successMessage = form.nextElementSibling;
+
+    form.addEventListener('submit', function(e) {
+      e.preventDefault();
+
+      const emailInput = this.querySelector('input[type="email"]');
+      const email = emailInput.value;
+
+      fetch('http://127.0.0.1:3000/mailing-list', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: email }),
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+        emailInput.value = '';
+        successMessage.style.display = 'block';
+        setTimeout(() => { successMessage.style.display = 'none'; }, 5000);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+    });
+  });
+});
